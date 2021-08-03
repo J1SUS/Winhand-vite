62fa5fda37e678ae76ae4b224326f6ee72a52486
J1SUS <jesusmanuelpolancogarcia51@gmail.com>
2021-08-03 16:24:56 -0400

    Added responsive navbar

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 import logo from './img/logo-principal-2.png'
 
 export default function Navbar() {
+  const myfunction = () => {
+    var x = document.getElementById("links");
+    if (x.className === "links") {
+      x.className += " responsive";
+    } else {
+      x.className = "links";
+    };
+    var i = document.getElementById("logo");
+    if (i.className === "logo") {
+      i.className += " responsive";
+    } else {
+      i.className = "logo";
+    }
+  }
+
+
     return (
-        <div className="navbar">
-            <img src={logo} alt="Logo" />
-            <div className="links">
-                <h2>REGISTRATE</h2>
-                <h1>NOSOTROS</h1>
+        <nav className="navbar">
+            <a href="#"><img src={logo} class="logo" id="logo" alt="Logo" /></a>
+            <h1 id="check" onClick={myfunction}>=</h1>
+            <div id="links" className="links">
+                <h2><a href="#">REGISTRATE</a></h2>
+                <h1><a href="#">NOSOTROS</a></h1>
             </div>
-        </div>
+        </nav>
     )
 }
 
