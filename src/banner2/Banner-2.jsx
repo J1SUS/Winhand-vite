44c5be469c564ac9ca0ad1d090cee34ad6c55f16
J1SUS <jesusmanuelpolancogarcia51@gmail.com>
2021-08-03 18:25:06 -0400

    remake of the Banner-2

--- src/banner2/Banner-2.jsx
+++ src/banner2/Banner-2.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Banner2.css';
-import banner from './img/ban2.png';
+import Donaplay from './img/donaplay.png'
 
 export default function Banner2() {
     return (
         <div className="Banner-2">
-            <img src={banner} alt="ban-2" />
+            <div className="text">
+              <h1>El Objetivo de WinHand es Ayudar al Prójimo</h1>
+              <p>50% Se Repartirán en Premios</p>
+              <p>45% Fondo de Ayudas Sociales</p>
+              <p>5% Administrativos de la Plataforma</p>
+          </div>
+          <img src={Donaplay} alt="Donaplay" />
         </div>
     )
 }
